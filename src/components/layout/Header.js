c489aa4950ed4ad30e4356dfc2016be2b512d07a
Andrew A. Stenipanin <and5251@yandex.ru>
2020-07-10 21:38:37 +0300

    scss styles

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,17 +1,21 @@
 import React from "react";
-import {FaPizzaSlice} from 'react-icons/fa'
+import { FaPizzaSlice } from "react-icons/fa";
 
 export const Header = () => {
   return (
     <header className="header" data-testid="header">
       <nav>
         <div className="logo">
-          <img src="" alt="" />
+          <img src="/images/logo.png" alt="Todoist" />
         </div>
         <div className="settings">
           <ul>
-            <li>+</li>
-            <li><FaPizzaSlice/></li>
+            <li data-testid="quick-add-task-action" className="settings__add">
+              +
+            </li>
+            <li data-testid="dark-mode-action" className="settings__darkmode">
+              <FaPizzaSlice />
+            </li>
           </ul>
         </div>
       </nav>
